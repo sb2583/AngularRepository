7b9d7d6b1cb4eaf89351da1baa0ceb200e53081f
sb2583 <ssb@ssb.com>
2016-10-10 21:27:41 +1100

    updated assignment further

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -4,35 +4,40 @@
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope', '$filter'];
-function LunchCheckController($scope, $filter) {
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
 
 $scope.checkIfTooMuch = function () {
 var data = $scope.lunchMenu;
+var lunchMenuList =arrayOfLunchMenu(data);
 
-if(data == null || data == "")
+if(lunchMenuList.length == 0)
 {
   $scope.response = "Please enter data first";
-  $scope.style = {color: 'red' };
+  $scope.responseColor  = 'red';
 }
-else
-{
-  var flag = data.endsWith(",");
-  if(flag) {
-    data = data.substring(0,data.length-1);
-  }
-  var lunchMenuList = data.split(",");
-  if(lunchMenuList.length > 3 ) {
-    $scope.response = "Too much!";
-    $scope.style = {color: 'green' };
-  } else {
+else if (lunchMenuList.length <= 3 ) {
     $scope.response =  "Enjoy!";
-    $scope.style = {color: 'green' };
+    $scope.responseColor = 'green';
+} else {
+    $scope.response = "Too much!";
+    $scope.responseColor = 'green';
   }
 }
+};
+function arrayOfLunchMenu(data){
+var menuList;
 
-
+var arrayList = new Array();
+if(data != null && data != '')
+{
+  menuList = data.split(",");
+  for (var i = 0; i < menuList.length; i++) {
+    if(menuList[i].length > 0)
+      arrayList.push(menuList[i]);
+  }
+}
+  return arrayList;
 }
 
-};
 })();
