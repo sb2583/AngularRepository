c7e07036274f37a5b5ec205563b1480f1fcec8c9
sb2583 <ssb@ssb.com>
2016-10-30 16:28:15 +1100

    Assignment 3

---
+++ module3-solution/app.js
@@ -0,0 +1,80 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
+.directive('foundItems', foundItemsDirective);
+
+function foundItemsDirective() {
+  var ddo = {
+    templateUrl: 'founditems.html',
+    scope: {
+      items: '<',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'list',
+    bindToController: true
+  };
+
+  return ddo;
+}
+
+function FoundItemsDirectiveController() {
+  var list = this;
+}
+
+
+NarrowItDownController.$inject = ['MenuSearchService']
+function NarrowItDownController(MenuSearchService) {
+  var list = this;
+  list.items = "";
+  list.errorMessage = "";
+  list.getMenuItems = function () {
+    var searchTerm = list.searchTerm;
+
+    var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+    promise.then(function (response) {
+       list.items = response;
+    })
+    .catch(function (error) {
+       console.log(error);
+    })
+  };
+
+  list.removeItem = function (itemIndex) {
+    MenuSearchService.removeItem(itemIndex);
+  };
+}
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath']
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+  var foundItems = [];
+
+  service.getMatchedMenuItems = function (searchTerm) {
+
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    }).then(function (result) {
+       foundItems = [];
+        for(var i=0; i<result.data.menu_items.length; i++){
+          if(result.data.menu_items[i].description.includes(searchTerm)) {
+            foundItems.push(result.data.menu_items[i]);
+          }
+        }
+      return foundItems;
+    });
+
+    return response;
+  };
+
+  service.removeItem = function (itemIndex) {
+      foundItems.splice(itemIndex, 1);
+    };
+}
+})();
